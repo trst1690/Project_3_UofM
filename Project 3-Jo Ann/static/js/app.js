267c6ebedd21e4a0e68fe8eb4fb8a50f57966919
Jo Ann Millegan <128994220+jmillegan@users.noreply.github.com>
2023-07-27 21:44:16 -0500

    Add files via upload
    Added updated and cleaned code and folders

--- Project 3-Jo Ann/static/js/app.js
+++ Project 3-Jo Ann/static/js/app.js
@@ -1,11 +1,13 @@
 d3.json("my_database.nps_data.json").then(function(data) {
    console.log(data);
-
+   
+   
 // Command to update the bar chart based on the State
 function optionChanged(state) {
-    doBarChart(state);
-    // doBubbleChart(state_id);
+        doBarChart(state);
+    
 }
+
 
 // Create barchart
 function doBarChart(state) {
@@ -14,8 +16,9 @@ function doBarChart(state) {
     let statesdata = data.filter(s => s.states === state);
     console.log(statesdata);
 
-    stateList = []
-    stateCount ={}
+    stateList = [] //list of type of parks
+    stateCount ={}//count of parks
+    
 
     statesdata.forEach(st_abb => {
       if (!stateList.includes(st_abb.designation)) {
@@ -24,7 +27,9 @@ function doBarChart(state) {
             }
             stateCount[st_abb.designation]++
     })
+
     console.log(stateCount)
+    console.log(stateList)
 
     dotview = []
     Object.keys(stateCount).forEach(key =>{
@@ -35,106 +40,41 @@ function doBarChart(state) {
     let trace = {
       y: stateCount,
       x: stateList,
-      type: "bar"
-      // orientation: "h"
+      type: "bar",
+      orientation: "v"
     };
 
     // Create data array
     let traces = [trace];
-    let layout = { title: "Parks" };  
-
+    let layout = {
+      title: "Parks",
+      xaxis: {
+        tickangle: -45 
+      }
+    };
     // Create Plotly chart
     Plotly.newPlot("bar", traces, layout);
   }
-  doBarChart('CA')
+  doBarChart();
 
+  let dropdown = document.getElementById("selDataset");
+  dropdown.addEventListener("change", function() {
+    const selectedState = dropdown.value;
+    optionChanged(selectedState);
+  });
 
-// Fetch JSON data to populate dropdown
-// d3.json("my_database.nps_data.json").then(function(result) {
-  
-  //////////////////////////////CREATE DROP DOWN????////////////////////////////////////////
-//   const dropdown = d3.select("#selDataset");
-//   const stateList = data.states.map(state => state.state);
-
-//   // Add dropdown options
-//   dropdown
-//     .selectAll("option")
-//     .data(stateList)
-//     .enter()
-//     .append("option")
-//     .text(function(d) {
-//       return d;
-//     });
-
-//   // Add event listener to the dropdown
-//   dropdown.on("change", function() {
-//     const selectedState = dropdown.property("value");
-//     optionChanged(selectedState);
-//   });
-
-//   // Call the updateBarChart function with the initial sample_id
-//   optionChanged(stateList[0]);
-// }).catch(function(error) {
-//   console.error(error);
-
-});
-
-//Promise pending  BUBBLE CHART
-//  function doBubbleChart(state) {
-//     stateList = []
-//     stateCount ={}
-
-//     let statesdata = data.filter(s => s.states === state);
-//     statesdata.forEach(st_abb => {
-//       if (!stateList.includes(st_abb.designation)) {
-//         stateList.push(st_abb.designation);
-//         stateCount[st_abb.designation]=0
-//             }
-//       stateCount[st_abb.designation]++
-//     })
-//     console.log(stateCount)
-//     dotview = []
-//     Object.keys(stateCount).forEach(key =>{
-//       dotview.push(stateCount[key])
-//     })
-  
-//     let trace2 = {
-//         x: stateList,
-//         y: dotview,
-//         mode: 'markers',
-//         marker: {
-//           size: dotview * 500,
-//           color: stateList,
-//           colorscale: "Earth"
-//         },
-//         text: stateList
-//       };
+  // Define the data variable to hold fetched data
     
-//       let data1 = [trace2];
+    data.forEach((name) => {
+      let subjectId = document.createElement("option");
+      subjectId.text = name.states; 
+      subjectId.value = name.states; 
+      dropdown.append(subjectId); 
       
-//       let layout = {
-//         showlegend: false,
-//         height: 500,
-//         width: 1000,
-//         xaxis: {
-//           title: 'Park Types'
-//         },
-        
-//       };
-    
-//       Plotly.newPlot("bubble", data1, layout);
-//     }
-
-//   doBubbleChart('LA')
-// //Create Demographics information box
-//       let metadataDiv = d3.select("#sample-metadata");
-//       metadataDiv.html("");
-//       console.log(result);
-//       let alldata = result.metadata;
-//       let selectedMetadata = alldata.filter(metadata => metadata.id == sample_id);
-//       console.log(selectedMetadata);
-//       Object.entries(selectedMetadata[0]).forEach(([key, value]) => {
-//         metadataDiv.append("p").text(`${key}: ${value}`);
-//       })
+      
+    });
+   
+  })
 
 
+  
